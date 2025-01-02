885303cda7eea9ca009342d507730d1736e17d9b
louilat <lmm.latournerie@gmail.com>
2025-01-02 10:44:26 +0100

    add log debug

--- reserves_data_etl.py
+++ reserves_data_etl.py
@@ -31,6 +31,10 @@ AWS_SECRET_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 AWS_SESSION_TOKEN = os.environ["AWS_SESSION_TOKEN"]
 POOL_ADDRESSES_PROVIDER_MAINNET = "0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e"
 
+logger.log(f"key = {AWS_ACCESS_KEY}")
+logger.log(f"sec = {AWS_SECRET_KEY}")
+logger.log(f"tok = {AWS_SESSION_TOKEN}")
+
 client_s3 = boto3.client(
     "s3",
     endpoint_url="https://" + "minio.lab.sspcloud.fr",
